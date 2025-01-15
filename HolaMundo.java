2548f58efffbfd2d409ccaca630a0cf9887d80a2
ES\fgonzalezcastro <fgonzalezcastro@deloitte.es>
2025-01-15 13:15:19 +0100

    Mensaje personalizado feature1

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 // Un programa simple en Java
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
+        System.out.println("¡Hola Mundo de feature1!");
     }
 }
